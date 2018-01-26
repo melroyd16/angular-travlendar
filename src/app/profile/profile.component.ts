f80320ad9637cbf41f58cee308e32418192d8de4
Param Mehta <param.mehta95@gmail.com>
2018-01-25 20:00:36 -0700

    Changes to Profile Page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { UserLoginService } from '../services/user-login.service';
 import { LoggedInCallback } from '../services/cognito.service';
 import { ProfileService } from './shared/profile.service';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -14,7 +15,23 @@ export class ProfileComponent implements OnInit {
 
   homeLocationText: string;
   workLocationText: string;
-
+  public someRange2: number[] = [10, 15];
+  public someRange2config: any = {
+    behaviour: 'drag',
+    connect: true,
+    margin: 1,
+    limit: 5,
+    pageSteps: 10,  // number of page steps, defaults to 10
+    range: {
+      min: 11,
+      max: 15
+    },
+    pips: {
+      mode: 'steps',
+      density: 5,
+      values: 6,
+    }
+  };
   constructor(public router: Router, public userService: UserLoginService, public profileService: ProfileService) {
     this.userService.isAuthenticated(this);
   }
