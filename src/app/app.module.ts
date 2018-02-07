5041f21aece49f5a7c2514b39af1b94136c3816b
Melroy Dmello <m_dmello@live.com>
2018-02-06 19:10:47 -0700

    Revert "Merge pull request #1 from melroyd16/param"
    This reverts commit b2c9bb47df5d18059c17d40947024fb7628ca53b, reversing
    changes made to 0036a697d62916fa59b227f92eca2814c2c9db25.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,8 @@ import { EventsService } from './calendar-view/shared/events.service';
 import * as $ from 'jquery';
 import * as bootstrap from 'bootstrap';
 
+
+
 import { AboutComponent, HomeComponent, HomeLandingComponent } from './public/home.component';
 import { LoginComponent } from './public/auth/login/login.component';
 import { RegisterComponent } from './public/auth/register/registration.component';
@@ -37,7 +39,6 @@ import { LogoutComponent, RegistrationConfirmationComponent } from './public/aut
 import { ResendCodeComponent } from './public/auth/resend/resendCode.component';
 import { NewPasswordComponent } from './public/auth/newpassword/newpassword.component';
 import { AuthInterceptor } from './apiinterceptor';
-import {IonRangeSliderModule} from 'ng2-ion-range-slider';
 
 @NgModule({
   declarations: [
@@ -66,7 +67,6 @@ import {IonRangeSliderModule} from 'ng2-ion-range-slider';
     BrowserAnimationsModule,
     CommonModule,
     FormsModule,
-    IonRangeSliderModule,
     HttpModule,
     HttpClientModule,
     routing
