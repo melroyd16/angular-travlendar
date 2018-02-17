e00c93aa2d8fce6d8fac7c69ae45f9ef0b911537
Aditya Nimbalkar <aditya.nimbalkar@asu.edu>
2018-02-16 20:21:02 -0700

    TG-168 #ready-for-test

--- src/app/calendar-view/calendar-view.component.ts
+++ src/app/calendar-view/calendar-view.component.ts
@@ -15,7 +15,8 @@ import {
   endOfMonth,
   isSameDay,
   isSameMonth,
-  addHours
+  addHours,
+  isPast
 } from 'date-fns';
 import { Subject } from 'rxjs/Subject';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -318,7 +319,10 @@ export class CalendarViewComponent implements OnInit {
         beforeStart: true,
         afterEnd: true
       },
-      actions: this.eventActions
+      // PREVENT EDIT/DELETION OF PAST EVENTS
+
+      actions: this.getEventActions(eventTitle, eventEnd)
+      // actions: this.eventActions
     });
     this.refresh.next();
   }
@@ -344,6 +348,14 @@ export class CalendarViewComponent implements OnInit {
         }
       }
     });
+  }
+
+  getEventActions(eventTitle, eventEndTime): CalendarEventAction[] {
+    console.log(eventTitle, eventEndTime);
+    console.log(isPast(eventEndTime))
+    if (isPast(eventEndTime)) {
+      return [];
+    } else return this.eventActions;
   }
 
 
