b67de63e470df0a943efe1f42673977df7e8ad87
Aditya Nimbalkar <aditya.nimbalkar@asu.edu>
2018-04-17 01:02:34 -0700

    TG-280 - #ready-for-test Changed Event-Title & Time format

--- src/app/calendar-view/custom-event-title-formatter.provider.ts
+++ src/app/calendar-view/custom-event-title-formatter.provider.ts
@@ -10,26 +10,26 @@ export class CustomEventTitleFormatter extends CalendarEventTitleFormatter {
   // you can override any of the methods defined in the parent class
 
   month(event: CalendarEvent): string {
-    return `<b>${new DatePipe(this.locale).transform(
+    return `${new DatePipe(this.locale).transform(
       event.start,
       'h:m a',
       this.locale
-    )}</b> ${event.title}`;
+    )} - ${event.title}`;
   }
 
   week(event: CalendarEvent): string {
-    return `<b>${new DatePipe(this.locale).transform(
+    return `${new DatePipe(this.locale).transform(
       event.start,
       'h:m a',
       this.locale
-    )}</b> ${event.title}`;
+    )} - ${event.title}`;
   }
 
   day(event: CalendarEvent): string {
-    return `<b>${new DatePipe(this.locale).transform(
+    return `${new DatePipe(this.locale).transform(
       event.start,
       'h:m a',
       this.locale
-    )}</b> ${event.title}`;
+    )} - ${event.title}`;
   }
 }
